4ffa4f3af83365286f5bf411b6a772423ed7f600
Redtails <najebjohnson@gmail.com>
2023-02-20 11:54:12 -0800

    working to-do list

--- todo.js
+++ todo.js
@@ -1,15 +1,8 @@
-const taskInput = document.getElementById("task");
-const taskList = document.getElementById("taskList");
-
-// Function to add a new task to the list
 function addTask() {
-  // Get the value of the task input
-  const newTask = taskInput.value;
-
-  // Create a new list item element
-  const listItem = document.createElement("li");
-
-  // Add the new task as text to the list item
-  listItem.appendChild(document.createTextNode(newTask));
-
-  // Add the new list item
+  var taskInput = document.getElementById("new-task");
+  var taskList = document.getElementById("task-list");
+  var task = document.createElement("li");
+  task.innerText = taskInput.value;
+  taskList.appendChild(task);
+  taskInput.value = "";
+}
